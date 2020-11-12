4af3cfa9710ccf6bf7f9d6c8c7e1bebf5fbf10f0
Hyun <32308475+citation0097@users.noreply.github.com>
2020-11-11 21:24:19 -0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 
- [ { "id" : "01" ,  
+ var a =
+  [ { "id" : "01" ,  
     "deptname" : "HR" , 
     "name" : "Aai" , 
     "bsalary" : "100000" ,  
@@ -11,10 +12,24 @@
     "bsalary" : "100000" ,  
     "ssalary" : "900" 
 },
-{ "id" : "03" ,  
+{  "id" : "03" ,  
     "deptname" : "WH" , 
     "name" : "Cee" , 
     "bsalary" : "90000" ,  
     "ssalary" : "900" 
 }  ]
-
+var grades = { 
+   'list' : {'e':10 , 'k':6 , 'm':7},
+   'show' : function(){
+       for(var name in this.list){
+           console.log(name , this.list[name]);
+       }
+   }
+}
+grades['list']['e']; 
+grades['show']();
+grades.show();
+var list = {'e':10 , 'k':6 , 'm':7};
+for(var key in list){
+    document.write("key:" +key + "value:"+ list[key] + "<br/>");
+}
