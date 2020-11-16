bb162499aea421edfe316bc3e97810251f770ca8
Hyun <32308475+citation0097@users.noreply.github.com>
2020-11-15 18:09:57 -0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,35 +1,22 @@
-
- var a =
-  [ { "id" : "01" ,  
-    "deptname" : "HR" , 
-    "name" : "Aai" , 
-    "bsalary" : "100000" ,  
-    "ssalary" : "900" 
+var a =
+ [ { "id" : "01" ,  
+   "deptname" : "HR" , 
+   "name" : "Aai" , 
+   "bsalary" : "100000" ,  
+   "ssalary" : "900" 
 } ,
 { "id" : "02" ,  
-    "deptname" : "IT" , 
-    "name" : "Bee" , 
-    "bsalary" : "100000" ,  
-    "ssalary" : "900" 
+   "deptname" : "IT" , 
+   "name" : "Bee" , 
+   "bsalary" : "100000" ,  
+   "ssalary" : "900" 
 },
 {  "id" : "03" ,  
-    "deptname" : "WH" , 
-    "name" : "Cee" , 
-    "bsalary" : "90000" ,  
-    "ssalary" : "900" 
+   "deptname" : "WH" , 
+   "name" : "Cee" , 
+   "bsalary" : "90000" ,  
+   "ssalary" : "900" 
 }  ]
-var grades = { 
-   'list' : {'e':10 , 'k':6 , 'm':7},
-   'show' : function(){
-       for(var name in this.list){
-           console.log(name , this.list[name]);
-       }
-   }
-}
-grades['list']['e']; 
-grades['show']();
-grades.show();
-var list = {'e':10 , 'k':6 , 'm':7};
-for(var key in list){
-    document.write("key:" +key + "value:"+ list[key] + "<br/>");
-}
+
+
+var  array =  {"title": "javascript", "author" : "hank"}
