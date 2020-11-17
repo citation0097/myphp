280b3733334932007d9a1740c73601928a79806b
Hyun <32308475+citation0097@users.noreply.github.com>
2020-11-16 22:56:17 -0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,22 +1,28 @@
-var a =
- [ { "id" : "01" ,  
-   "deptname" : "HR" , 
-   "name" : "Aai" , 
-   "bsalary" : "100000" ,  
-   "ssalary" : "900" 
-} ,
-{ "id" : "02" ,  
-   "deptname" : "IT" , 
-   "name" : "Bee" , 
-   "bsalary" : "100000" ,  
-   "ssalary" : "900" 
-},
-{  "id" : "03" ,  
-   "deptname" : "WH" , 
-   "name" : "Cee" , 
-   "bsalary" : "90000" ,  
-   "ssalary" : "900" 
-}  ]
+
+var  arr = new Array ["a", "javascript", "author" , "hank"];
+
+//  arr = ['seoul', 'new yourk', 'pusan'];
+function getRandom(arr){
+   var index = arr.length*Math.random();
+   return arr[index];
+}
+
+Array.prototype.random = function(){
+   var index = Math.floor(this.length*Math.random());
+   return this[index];
+}
+var that = new Array( 'a', 'f', 'c');
+that.random();
+// mozila develop
+
+Object.prototype.contain = function(needle){
+   for(var name in this){
+      if(this[name] === needle){
+         return true;   
+      }
+   }
+   return false;
+}
 
 
-var  array =  {"title": "javascript", "author" : "hank"}
+
